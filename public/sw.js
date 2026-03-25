7b79de29c3ad8e44e36a34087d4f718a09ccc231
Huy <hoanghuy1kt@gmail.com>
2026-03-25 13:01:37 +0700

    update 1

---
+++ public/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'trading-pro-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  // Network first strategy for dynamic app
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
